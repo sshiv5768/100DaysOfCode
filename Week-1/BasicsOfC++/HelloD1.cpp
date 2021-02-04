86ff4cd1a48ccd5f55ba2c9cffb96e2827a02d92
Shivlal <sshivlal9601@gmail.com>
2021-02-04 17:58:27 +0530

    Day-1

---
+++ Week-1/BasicsOfC++/HelloD1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>  // Header file
+using namespace std; // Standard c++ namespace
+
+int main() // Start
+{
+    // Day1
+    /* Output */
+    cout << "Hello, everyone!" << endl;
+    cout << "Welcome to #100DaysOfCode";
+    return 0; // End
+}
