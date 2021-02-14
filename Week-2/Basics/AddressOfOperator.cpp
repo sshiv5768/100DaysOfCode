c771dbb07b84bbedf2446cc8d6090b4d214c1df5
Shivlal <sshivlal9601@gmail.com>
2021-02-14 09:46:19 +0530

    Day-9 AddressOf Operator

---
+++ Week-2/Basics/AddressOfOperator.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 34;
+    char ch = 'A';
+    // Find the address of int a in memory.
+    cout << "The address of a: " << &a << endl;
+
+    // Find the address of char ch in memory.
+    // IT dosen't work for character values.
+    cout << "The address of ch: " << &ch << endl;
+
+    // We can use explicit way.
+    cout << "The address of ch: " << (void *)&ch << endl;
+    return 0;
+}
