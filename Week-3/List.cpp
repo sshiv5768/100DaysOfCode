34ec61bf082f9de137ff51a222e9d2674d344f57
Shivlal <sshivlal9601@gmail.com>
2021-03-08 23:42:13 +0530

    List & List operations

--- Week-3/List.cpp
+++ Week-3/List.cpp
@@ -6,7 +6,26 @@ using namespace std;
 int main()
 {
     list<int> l1{1, 2, 3, 4, 5};
+    list<string> l2{"Apple", "Banana"};
 
+    l2.push_back("Mango");
+
+    for (string i : l2)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    // Reverse the list
+    l2.reverse();
+    for (string i : l2)
+    {
+        cout << i << " ";
+    }
+
+    cout << endl;
+    // Inserting elements to the list
+    l1.insert(l1.begin(), 1);
     for (int i : l1)
     {
         cout << i << " ";
