68d9999118f95558f99949c1c007ba6663e507e7
Shivlal <sshivlal9601@gmail.com>
2021-03-08 23:20:01 +0530

    List

---
+++ Week-3/List.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+int main()
+{
+    template <typename T>
+    ;
+    list<int> l1{1, 2, 3, 4, 5};
+
+    for (int i : l1)
+    {
+        cout << i << " ";
+    }
+
+    return 0;
+}
